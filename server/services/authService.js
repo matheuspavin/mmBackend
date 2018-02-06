4d37df92921f0bf8a1b3c36405e614e150327adf
matheuspavin <pavinmatheus@gmail.com>
2018-02-05 22:58:31 -0200

    Adjusting session

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,47 +1,47 @@
 const jwt = require('jsonwebtoken');
 
-// const getSession = async (userToken) => {
-//     let user = await userService.getUserAndDetails(userToken.idUser);
-//     user.language = userToken.language;
-//     const customers = await customerService.getCustomersByUser(user.idUser);
+const getSession = async (userToken) => {
+    let user = await userService.getUserAndDetails(userToken.idUser);
+    user.language = userToken.language;
+    const customers = await customerService.getCustomersByUser(user.idUser);
 
-//     if (!user.idActiveCustomer) {
-//         userService.changeActiveCustomer(user.idUser, customers[0].idCustomer);
-//     }
+    if (!user.idActiveCustomer) {
+        userService.changeActiveCustomer(user.idUser, customers[0].idCustomer);
+    }
 
-//     return {
-//         user,
-//         customers
-//     }
-// };
+    return {
+        user,
+        customers
+    }
+};
 
 const isAuthenticated = function (request, response, next) {
-    // let token = request.headers['x-access-token'];
-    // jwt.verify(token, security.certificate, async (err, tokenUser) => {
-    //     if (err) {
-    //         response.status(401).end();
-    //     } else {
-    //         await createUserDetailsIfDoesntExist(tokenUser.idUser);
-    //         request.user = await userService.getUserAndDetails(tokenUser.idUser);
-    //         request.user.language = tokenUser.languageCode;
-    //         next();
-    //     }
-    // });
+    let token = request.headers['x-access-token'];
+    jwt.verify(token, security.certificate, async (err, tokenUser) => {
+        if (err) {
+            response.status(401).end();
+        } else {
+            await createUserDetailsIfDoesntExist(tokenUser.idUser);
+            request.user = await userService.getUserAndDetails(tokenUser.idUser);
+            request.user.language = tokenUser.languageCode;
+            next();
+        }
+    });
     next();
 };
 
-// const createUserDetailsIfDoesntExist = async function (idUser) {
-//     const userDetails = await userService.getUserDetails(idUser);
-//     if (!userDetails) {
-//         await userService.saveUserDetails(idUser);
-//     }
-// };
+const createUserDetailsIfDoesntExist = async function (idUser) {
+    const userDetails = await userService.getUserDetails(idUser);
+    if (!userDetails) {
+        await userService.saveUserDetails(idUser);
+    }
+};
 
-// const hasActiveCustomer =  (req, res, next) => {
-//     let user = req.user;
-//     req.customer = { id: user.idActiveCustomer };
-//     next();
-// };
+const hasActiveCustomer =  (req, res, next) => {
+    let user = req.user;
+    req.customer = { id: user.idActiveCustomer };
+    next();
+};
 
 // FIXME: Autorização desabilitada temporariamente!!!
 const isAuthorized = function (req, res, next) {
