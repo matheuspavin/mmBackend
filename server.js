8cc5dd0594561729a67bcd60394f83021de099e8
matheuspavin <pavinmatheus@gmail.com>
2018-03-04 20:31:52 -0300

    Starting routing

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const banco = require("./connectDataBase.js");
 const bodyParser = require('body-parser');
 const sessionService = require('./service/sessionService.js');
 
+var customerRoute = require('./routes/customerRoute')
+var garageRoute = require('./routes/garageRoute')
+var sessionRoute = require('./routes/sessionRoute')
+var usersRoute = require('./routes/usersRoute')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -21,54 +26,7 @@ app.options('*', function (req, res, next) {
 });
 
 app.use('/', require('./routes/sessionRoute'));
-app.use('/', sessionService.isAuthenticated, require('./routes/garageRoute'));
-
-app.post('/insert/garage', function(req, res){
-	var garage = req.body;
-	banco.insertGarage(garage).then(function(result){
-		result = result[0];
-		var user = {};
-		user.id_garage = result.id;
-		user.password = garage.password;
-		banco.insertUser(user).then(function(result){
-			res.send(user);
-		})
-	})
-});
-
-app.post('/insert/customer', function(req, res){
-	var customer = req.body;
-	banco.insertCustomer(customer).then(function(result){
-		result = result[0];
-		var user = {};
-		user.id_customer = result.id;
-		user.password = customer.password;
-		banco.insertUser(user).then(function(result){
-			res.send(user);
-		})
-	})
-});
-
-app.get('/consultaCliente', function(req, res){
-	banco.listCliente().then(function(resultado){
-		res.send(resultado) ;
-	})
-});
-
-app.post('/adicionaPessoa', function(req, res){
-	const newPessoa = req.body;
-	banco.insertPessoa(newPessoa).then(function(resultado){
-		res.send(resultado) ;
-	})
-});
-
-app.post('/deletaPessoa', function(req, res){
-	const delPessoa = req.body;
-	banco.deletaPessoa(delPessoa).then(function(resultado){
-		res.send(resultado) ;
-	})
-});
-
+app.use('/', sessionService.isAuthenticated, require('./routes/garageRoute'))
 
 const server = app.listen(8081, function () {
 	console.log("meuMecanico rodando em modo desenvolvimento no ip: ", server.address().address, " e na porta", server.address().port);
