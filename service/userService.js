e0831a9a3b285b9f352291d80727d59791388fb6
matheuspavin <pavinmatheus@gmail.com>
2018-03-05 22:08:45 -0300

    Console.log

--- service/userService.js
+++ service/userService.js
@@ -1,7 +1,7 @@
 const databaseService = require('./databaseService');
 
 var listClient = function(){
-    var consult = "SELECT * FROM pessoa"
+    var consult = "SELECT * FROM mymechanic.user"
     return databaseService.get(consult, [])
 }
 
