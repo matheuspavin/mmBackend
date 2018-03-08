8754f077a65fe59975eff27dc7525e7eae008a2d
Matheus Pavin Pedroso <matheus@MacBook-Pro-de-Matheus.local>
2018-03-07 22:50:24 -0300

    changing the sql to the services

--- service/userService.js
+++ service/userService.js
@@ -6,6 +6,13 @@ var listClient = function(){
 }
 
 
+var insertPerson = function(person){
+    var query1 = "INSERT INTO person(name, info) VALUES($1,$2)";
+    var params = [person.name, person.info];
+    return query(query1, params)
+};
+
+
 module.exports = {
     listClient
 }
