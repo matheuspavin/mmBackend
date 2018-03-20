63984bc0458bf3c14f176bd2e07cbd83e2ac9017
matheuspavin <pavinmatheus@gmail.com>
2018-03-20 00:06:07 -0300

    Routing

--- gateways/carsGateway.js
+++ gateways/carsGateway.js
@@ -15,7 +15,7 @@ const getBrands = function (typeOfVehicle) {
 
 const getCars = function (typeOfVehicle, brand) {
     const request = {
-        url: `${config.cars.cars}${typeOfVehicle}/marcas`,
+        url: `${config.cars.brands}${typeOfVehicle}/marcas/${brand}/modelos`,
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=UTF-8',
--- routes/carsRoute.js
+++ routes/carsRoute.js
@@ -3,9 +3,17 @@ const carsService = require('../service/carsService');
 
 router.get('/brands', async function (req, res, next) {
     const type = req.query.type;
-    let result = await carsService.getBrands(type);
+    const result = await carsService.getBrands(type);
     return res.json(result);
 });
+
+router.get('/cars', async function (req, res, next) {
+    const type = req.query.type;
+    const brand = req.query.brand;
+    const result = await carsService.getCars(type, brand);
+    return res.json(result);
+});
+
 
 
 module.exports = router;
